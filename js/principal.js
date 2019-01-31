6a9dba1944db1cb5a4d8f21bbe31daadf57757ba
davidrodriguezonetec <david.rodriguez@grupoonetec.com>
2019-01-31 12:52:05 +0100

    Fin ejemplo v-if

--- js/principal.js
+++ js/principal.js
@@ -3,21 +3,6 @@ var app = new Vue({
     el: '#app',
     data: {
         lista: [
-            {
-                nombre: 'Prueba1',
-                promedio: '10',
-                estado: false
-            },
-            {
-                nombre: 'Prueba2',
-                promedio: '17',
-                estado: true
-            },
-            {
-                nombre: 'Prueba3',
-                promedio: '4',
-                estado: false
-            }
         ],
         nombre: '',
         promedio: ''
